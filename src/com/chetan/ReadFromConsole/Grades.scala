95f7036787514c6c47bf7ca7dd492f80f8b7e7fe
CsBigDataHub <kchetan.hadoop@gmail.com>
2017-10-01 17:32:30 -0500

    Added ReadDataFromConsiole and Grades.scala Files
    Signed-off-by: CsBigDataHub <kchetan.hadoop@gmail.com>

---
+++ src/com/chetan/ReadFromConsole/Grades.scala
@@ -0,0 +1,24 @@
+package com.chetan.ReadFromConsole
+
+import scala.io.StdIn._
+
+object Grades extends App {
+  var done = false
+  while (!done) {
+    //this readline can also be defined as scala.io.StdIn.readLine
+    var name = readLine("Enter the student name:")
+    println("Enter student score: ")
+    var score = readDouble()
+    println(
+      "you have received an " + (if (score > 90) "A"
+                                 else if (score > 80) "B"
+                                 else if (score > 70) "C"
+                                 else if (score > 60) "D"
+                                 else "F")
+    )
+
+    println("Enter true if done,false to continue: ")
+    done = readBoolean()
+
+  }
+}
