542eb94822bc546757ea36ee61c67e7e5320508b
CsBigDataHub <kchetan.hadoop@gmail.com>
2017-10-01 17:57:02 -0500

    Added ReadDataFromFile files
    Signed-off-by: CsBigDataHub <kchetan.hadoop@gmail.com>

---
+++ src/com/chetan/ReadFromFile/readFromFile.scala
@@ -0,0 +1,20 @@
+package com.chetan.ReadFromFile
+
+import io.Source._
+object readFromFile extends App {
+  val fileName = "/home/mypc/IdeaProjects/WorkSheetExample/src/com/chetan/ReadFromFile/rainbow.txt"
+
+  for (line <- fromFile(fileName).getLines()) {
+    println(line.toUpperCase)
+  }
+
+  val poemLine = fromFile("src/com/chetan/ReadFromFile/poem.txt").getLines().toList
+  poemLine.foreach(println)
+  println("#############################")
+  var poemLines = fromFile("src/com/chetan/ReadFromFile/poem.txt").getLines()
+  for (poem <- poemLines){
+    println(poem)
+  }
+
+
+}
